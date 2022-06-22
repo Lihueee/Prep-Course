c6f657f4fcd0c66e23351c66266e5bb5f5aae61d
Lihue <lihuemimendez@hotmail.com>
2022-06-22 12:42:08 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -246,7 +246,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") { return "Es vocal" }
   
+  { return "Dato incorrecto" }
+  
+ if (letra.legth > 1 ) 
+ { return "Dato incorrecto" 
 }
 
 
