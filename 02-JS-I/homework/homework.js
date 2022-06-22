06af6a1fd8912556e83834326bd7077abdeab809
Lihue <lihuemimendez@hotmail.com>
2022-06-22 13:47:18 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -157,17 +157,18 @@ function numeroRandom() {
   return Math.random ();
 }
 
-function esPositivo(numero)
+function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0)
+  if (numero > 0) {
   return  "es positivo"; }
   { if (numero < 0)
   return "es negativo"; }
   { if (numero === 0) 
   return false; 
+}
 }
 
 
